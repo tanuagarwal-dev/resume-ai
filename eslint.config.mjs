b6abd029e4dd57b7fe733ad1970c1f98ff1307ce
Tanu Agarwal <162132053+tanu-agarwal0101@users.noreply.github.com>
2025-12-21 20:41:00 +0530

    feat: remove ESLint configuration file

--- eslint.config.mjs
+++
@@ -1,7 +0,0 @@
-export default {
-  extends: ["next/core-web-vitals"],
-  rules: {
-    "react/no-unescaped-entities": "off",
-  },
-  ignorePatterns: [".next", "node_modules", "dist", "build"],
-};
